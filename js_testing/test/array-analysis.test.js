1284a993824f643fc38d84e2e260b2ccc4340d1f
Alex Sun <alexsun82@icloud.com>
2020-04-20 15:03:21 +0200

    refactoring

--- js_testing/test/array-analysis.test.js
+++ js_testing/test/array-analysis.test.js
@@ -12,7 +12,6 @@ describe('Array Analysis', () => {
     it("min works", () => {
         assert.propertyVal(analyze([1, 8, 3, 4, 2, 6]), "min", 1);
     });
-
     it("max works", () => {
         assert.propertyVal(analyze([1, 8, 3, 4, 2, 6]), "max", 8);
     });
