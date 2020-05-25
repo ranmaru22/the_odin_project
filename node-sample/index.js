4a98828f38855edb22deebe654942bf702a577f0
Alex Sun <alexsun82@icloud.com>
2020-05-25 11:04:39 +0200

    Initial commit node-sample

---
+++ node-sample/index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http.createServer((req, res) => {
+    const filePath = `.${url.parse(req.url, true).pathname}`;
+    fs.readFile(filePath, (err, file) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            return fs.readFile("./404.html", (err, file) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.write(file);
+                    return res.end();
+                }
+            });
+        } else {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(file);
+            return res.end();
+        }
+    });
+});
